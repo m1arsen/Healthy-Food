bebfa91575f025a6a0322216fdd1e242a008322e
m1arsen <m1arsen.web@gmail.com>
2024-09-23 19:34:05 +0300

    feat: adding server.php for POST queries in db.json and adding a response after sending the data

--- js/script.js
+++ js/script.js
@@ -205,4 +205,85 @@ document.addEventListener('DOMContentLoaded', () => {
       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
     })
   });
+
+  // Forms
+
+  const forms = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'img/form/spinner.svg',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    error: 'Что-то пошло не так'
+  }
+
+  forms.forEach(item => {
+    bindPostData(item);
+  });
+
+  const postData = async (url, data) => {
+    let res = await fetch(url, {
+      method: "POST",
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: data
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement('img');
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+        display: block;
+        margin: 0 auto;
+      `;
+      form.append(statusMessage);
+      form.insertAdjacentElement('afterend', statusMessage);
+
+      const formData = new FormData(form);
+
+      const json = JSON.stringify(Object.fromEntries(formData.entries()))
+
+      postData('http://localhost:3000/requests', json)
+      .then(data => {
+        console.log(data);
+        showThanksModal(message.success);
+        statusMessage.remove();
+      }).catch(() => {
+        showThanksModal(message.error);
+      }).finally(() => {
+        form.reset();
+      })
+    })
+  }
+
+  // спиннер и замена содержимого модалки после отправки данных
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+      <div class="modal__content">
+        <div class="modal__close" data-close>×</div>
+        <div class="modal__title">${message}</div>
+      </div>
+    `;
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+        thanksModal.remove();
+        prevModalDialog.classList.add('show');
+        prevModalDialog.classList.remove('hide');
+        closeModal();
+    }, 4000);
+  }
 })
